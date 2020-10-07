948be9e4009d3578416929402e0ce52a5931984a
AsFeanor <altugegesari@gmail.com>
2020-10-07 13:50:18 +0300

    first

---
+++ src/components/Todo.vue
@@ -0,0 +1,19 @@
+<template>
+  <li class="list-group-item d-flex justify-content-between">
+
+    <a href = "#" class ="delete-item">
+      <i class = "fa fa-remove"></i>
+    </a>
+
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TodoInterface.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container" style ="margin-top:20px;">
+
+    <div class="card row">
+      <div class="card-body">
+        <form id = "todo-form" name="form">
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <input
+                class="form-control"
+                type="text"
+                name="todo"
+                id = "todo"
+                placeholder="Enter a Todo">
+            </div>
+          </div>
+          <button type="submit" class="btn btn-danger">Add a Todo</button>
+        </form>
+        <hr>
+
+      </div>
+
+      <div class="card-body">
+
+        <h5 class="card-title" id = "tasks-title">Todos</h5>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <input
+              class="form-control"
+              type="text"
+              name="filter"
+              id = "filter"
+              placeholder="Search for a Todo">
+          </div>
+        </div>
+
+        <hr>
+        <ul class="list-group">
+          <Todo></Todo>
+        </ul>
+
+        <a id = "clear-todos" class="btn btn-dark" href="#">Clear All Todos</a>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Todo from '@/components/Todo.vue';
+
+export default {
+  name: 'TodoInterface',
+  components: Todo,
+};
+</script>
+
+<style scoped>
+
+</style>
