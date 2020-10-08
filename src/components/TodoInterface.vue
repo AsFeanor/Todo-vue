4677bb24e7ec8dcc76161ba892aa98b84e60a221
AsFeanor <altugegesari@gmail.com>
2020-10-08 17:47:08 +0300

    final

--- src/components/TodoInterface.vue
+++ src/components/TodoInterface.vue
@@ -3,18 +3,18 @@
 
     <div class="card row">
       <div class="card-body">
-        <form id = "todo-form" name="form">
+        <form id = "todo-form">
           <div class="form-row">
             <div class="form-group col-md-6">
               <input
                 class="form-control"
                 type="text"
-                name="todo"
-                id = "todo"
+                v-model="todoName"
+                @keyup.enter="addTodo"
                 placeholder="Enter a Todo">
             </div>
           </div>
-          <button type="submit" class="btn btn-danger">Add a Todo</button>
+          <button @click="addTodo" type="submit" class="btn btn-danger">Add a Todo</button>
         </form>
         <hr>
 
@@ -22,24 +22,30 @@
 
       <div class="card-body">
 
-        <h5 class="card-title" id = "tasks-title">Todos</h5>
+        <h5 class="card-title">Todos</h5>
         <div class="form-row">
           <div class="form-group col-md-6">
             <input
               class="form-control"
               type="text"
-              name="filter"
-              id = "filter"
+              v-model="search"
               placeholder="Search for a Todo">
           </div>
         </div>
 
         <hr>
         <ul class="list-group">
-          <Todo></Todo>
-        </ul>
+          <li
+            v-for="todo in filteredTodos" :key="todo.todo_id"
+            class="list-group-item d-flex justify-content-between">
+            {{ todo.description }}
+            <a @click="deleteTodo(todo.todo_id)" class ="delete-item">
+              <i class = "fa fa-remove"></i>
+            </a>
 
-        <a id = "clear-todos" class="btn btn-dark" href="#">Clear All Todos</a>
+          </li>
+        </ul>
+        <a id = "clear-todos" class="btn btn-dark">Clear All Todos</a>
       </div>
     </div>
 
@@ -47,11 +53,50 @@
 </template>
 
 <script>
-import Todo from '@/components/Todo.vue';
+import axios from 'axios';
 
 export default {
   name: 'TodoInterface',
-  components: Todo,
+
+  mounted() {
+    axios.get('http://localhost:5000/todos')
+      .then((response) => {
+        this.todos = response.data;
+      })
+      .catch((error) => {
+        this.todos = console.log(error);
+      });
+  },
+  data() {
+    return {
+      todos: [],
+      todoName: '',
+      search: '',
+    };
+  },
+  methods: {
+    addTodo() {
+      axios.post('http://localhost:5000/todos', { description: this.todoName })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((e) => console.log(e));
+    },
+    deleteTodo(todoId) {
+      axios.delete(`http://localhost:5000/todos/${todoId}`)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  },
+  computed: {
+    filteredTodos() {
+      return this.todos.filter(({ description }) => description.match(this.search));
+    },
+  },
 };
 </script>
 
