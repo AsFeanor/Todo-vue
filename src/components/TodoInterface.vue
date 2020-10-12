3af62fb6caed748d102ba807aebd4838f4529d07
AsFeanor <altugegesari@gmail.com>
2020-10-12 12:29:00 +0300

    Probably Last

--- src/components/TodoInterface.vue
+++ src/components/TodoInterface.vue
@@ -3,25 +3,35 @@
 
     <div class="card row">
       <div class="card-body">
-        <form id = "todo-form">
+        <form @submit.prevent="submit">
           <div class="form-row">
-            <div class="form-group col-md-6">
+            <div class="form-group col-md-6" :class="{ 'form-group--error': $v.todoName.$error }">
               <input
                 class="form-control"
                 type="text"
-                v-model="todoName"
-                @keyup.enter="addTodo"
+                v-model.trim="$v.todoName.$model"
                 placeholder="Enter a Todo">
             </div>
+            <div class="error"
+                 v-if="!$v.todoName.required">Todo is required</div>
+            <div class="error"
+                 v-if="!$v.todoName.minLength">
+              Todo must have at least {{$v.todoName.$params.minLength.min}} letters.
+            </div>
           </div>
-          <button @click="addTodo" type="submit" class="btn btn-danger">Add a Todo</button>
+          <button @click="submit"
+                  type="submit"
+                  :disabled="submitStatus === 'PENDING'"
+                  class="btn btn-danger">Add a Todo</button>
+          <p class="typo__p" v-if="submitStatus === 'OK'">Thanks for your submission!</p>
+          <p class="typo__p" v-if="submitStatus === 'ERROR'">Please fill the blank correctly.</p>
+          <p class="typo__p" v-if="submitStatus === 'PENDING'">Sending...</p>
         </form>
         <hr>
 
       </div>
 
       <div class="card-body">
-
         <h5 class="card-title">Todos</h5>
         <div class="form-row">
           <div class="form-group col-md-6">
@@ -34,18 +44,20 @@
         </div>
 
         <hr>
-        <ul class="list-group">
+        <ul class="list-group" id="todoList">
           <li
             v-for="todo in filteredTodos" :key="todo.todo_id"
             class="list-group-item d-flex justify-content-between">
             {{ todo.description }}
-            <a @click="deleteTodo(todo.todo_id)" class ="delete-item">
+            <a @click="deleteTodo(todo.todo_id)" class ="delete-item" :href="'/'">
               <i class = "fa fa-remove"></i>
             </a>
-
           </li>
         </ul>
-        <a id = "clear-todos" class="btn btn-dark">Clear All Todos</a>
+        <a @click="clearTodos(todos)"
+           :href="'/'"
+           class="btn btn-dark">
+          Clear All Todos</a>
       </div>
     </div>
 
@@ -54,6 +66,7 @@
 
 <script>
 import axios from 'axios';
+import { required, minLength } from 'vuelidate/lib/validators';
 
 export default {
   name: 'TodoInterface',
@@ -63,8 +76,8 @@ export default {
       .then((response) => {
         this.todos = response.data;
       })
-      .catch((error) => {
-        this.todos = console.log(error);
+      .catch((e) => {
+        this.todos = console.log(e);
       });
   },
   data() {
@@ -72,18 +85,44 @@ export default {
       todos: [],
       todoName: '',
       search: '',
+      submitStatus: null,
     };
   },
-  methods: {
-    addTodo() {
-      axios.post('http://localhost:5000/todos', { description: this.todoName })
-        .then((response) => {
-          console.log(response);
-        })
-        .catch((e) => console.log(e));
+  validations: {
+    todoName: {
+      required,
+      minLength: minLength(4),
     },
+  },
+  methods: {
     deleteTodo(todoId) {
       axios.delete(`http://localhost:5000/todos/${todoId}`)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+    submit() {
+      console.log('submit!');
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        this.submitStatus = 'ERROR';
+      } else {
+        axios.post('http://localhost:5000/todos', { description: this.todoName })
+          .then((response) => {
+            console.log(response);
+          })
+          .catch((e) => console.log(e));
+        this.submitStatus = 'PENDING';
+        setTimeout(() => {
+          this.submitStatus = 'OK';
+        }, 500);
+      }
+    },
+    clearTodos() {
+      axios.delete('http://localhost:5000/todos')
         .then((res) => {
           console.log(res);
         })
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue';
+import Vuelidate from 'vuelidate';
 import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import router from './router';
 import store from './store';
+
+Vue.use(Vuelidate);
 
 Vue.config.productionTip = false;
 
